d20e37380e7b33ffa287cb3e0863ccfc37759f97
Tran Tien Dat <dattrantien654@gmail.com>
2024-10-30 22:49:41 +0700

    video 67

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import vn.hoidanit.laptopshop.domain.User;
@@ -43,10 +44,18 @@ public class UserController {
         return "admin/user/create";
     }
 
+    @RequestMapping("/admin/user/{id}")
+    public String getUserDetailPage(Model model, @PathVariable long id) {
+        User user = this.userService.getUserById(id);
+        model.addAttribute("user", user);
+        model.addAttribute("id", id);
+        return "admin/user/show";
+    }
+
     @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
     public String createUserPage(Model model, @ModelAttribute("newUser") User trandat) {
         this.userService.handleSaveUser(trandat);
-        return "redirect:/admin/user";
+        return "redirect:/admin/user"; // quay luon lai trang
     }
 
 }
