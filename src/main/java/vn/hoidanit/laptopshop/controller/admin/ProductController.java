bfee3c592250215aba713eb81cbae3e58cfe47ca
tdat0411 <dattrantien654@gmail.com>
2024-11-13 18:21:27 +0700

    video 103

--- src/main/java/vn/hoidanit/laptopshop/controller/admin/ProductController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/admin/ProductController.java
@@ -1,16 +1,38 @@
 package vn.hoidanit.laptopshop.controller.admin;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import jakarta.validation.Valid;
 import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.service.ProductService;
+import vn.hoidanit.laptopshop.service.UploadService;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class ProductController {
+    private final ProductService productService;
+    private final UploadService uploadService;
+
+    public ProductController(ProductService productService, UploadService uploadService) {
+        this.productService = productService;
+        this.uploadService = uploadService;
+    }
 
     @GetMapping("/admin/product")
-    public String getProduct() {
+    public String getProduct(Model model) {
+        List<Product> prs = this.productService.fetchProducts();
+        model.addAttribute("products", prs);
         return "admin/product/show";
     }
 
@@ -19,4 +41,18 @@ public class ProductController {
         model.addAttribute("newProduct", new Product());
         return "admin/product/create";
     }
+
+    @PostMapping("/admin/product/create")
+    public String handleCreateProduct(Model model, @ModelAttribute("newProduct") @Valid Product trandat,
+            BindingResult newProductBindingResult, @RequestParam("tiendatFile") MultipartFile file) {
+        // validate
+        if (newProductBindingResult.hasErrors()) {
+            return "admin/product/create";
+        }
+        String product = this.uploadService.handleSaveUploadFile(file, "product");
+        trandat.setImage(product);
+        this.productService.createProduct(trandat);
+        return "redirect:/admin/product";
+    }
+
 }
--- src/main/java/vn/hoidanit/laptopshop/repository/ProductRepository.java
+++ src/main/java/vn/hoidanit/laptopshop/repository/ProductRepository.java
@@ -2,10 +2,9 @@ package vn.hoidanit.laptopshop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import vn.hoidanit.laptopshop.domain.Role;
+import vn.hoidanit.laptopshop.domain.Product;
 
 @Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String name);
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
 }
